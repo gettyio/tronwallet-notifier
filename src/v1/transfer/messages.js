d954b8b48ec626e3a69f2883b6ec91e375f21364
Marlon Gomes <marlonbrgomes@gmail.com>
2018-05-14 12:34:18 -0300

    remove transfer

--- src/v1/transfer/messages.js
+++
@@ -1,2 +0,0 @@
-export const INVALID_XDR = 'Received XDR is invalid, please check the payload and retry.';
-export const INVALID_PUBLIC_KEY = 'Received public key is invalid, please check the payload and retry.';
